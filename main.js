d3c1f7c45ffed5c8c29ea33f04fd21b884cf28ff
Zac Colley <zac@clubpenguin.email>
2021-12-31 01:38:41 +0000

    fix endpoint failing

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ import fetch from "node-fetch";
 import Launchpad from "./launchpad.js";
 import Joycons from "./joycons.js";
 
-const { WSL_PORT } = process.env;
+const { NGROK_URL } = process.env;
+
+if (!NGROK_URL) {
+  throw new Error("Missing environment variabes.");
+}
 
 const launchpad = new Launchpad();
 const joycons = new Joycons();
@@ -15,17 +19,15 @@ launchpad.on("change", (data) => sendDataToMainServer("launchpad", data));
 joycons.on("change", (data) => sendDataToMainServer("joycons", data));
 
 async function sendDataToMainServer(eventType, data) {
+  console.log(eventType, data);
   try {
-    const response = await fetch(
-      `http://localhost:${WSL_PORT}/windows/${eventType}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ data }),
-      }
-    );
+    const response = await fetch(`${NGROK_URL}/windows/${eventType}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ data }),
+    });
     const json = await response.json();
     console.log("json", json);
   } catch (e) {
